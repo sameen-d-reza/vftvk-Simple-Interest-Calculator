9c715e7914a3b73636bc5b4a4b1321c4006e8184
sameen-d-reza <sameen.daiyan.reza@gmail.com>
2021-08-16 20:13:34 +0000

    configurations to Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = (document.getElementById("principal").value);
+    var rate = (document.getElementById("rate").value);
+    var years = (document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + year + "</mark>";
+
 }
-        
+
+function getSliderValue() {
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value;
+}
+
+
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+}
